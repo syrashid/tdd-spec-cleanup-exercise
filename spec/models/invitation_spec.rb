00820dcce032cfe126b145e616bc0c0766885ed8
Sy Rashid <sy.rashid.11@gmail.com>
2020-10-14 17:36:19 +0300

    FEAT: Set up first and second test

--- spec/models/invitation_spec.rb
+++ spec/models/invitation_spec.rb
@@ -1,34 +1,56 @@
 require "rails_helper"
 
 RSpec.describe Invitation do
-  let(:invitation) { build(:invitation, team: team, user: new_user) }
-  let(:new_user) { create(:user, email: "rookie@example.com") }
-  let(:team) { create(:team, name: "A fine team") }
-  let(:team_owner) { create(:user) }
+  # let(:invitation) { build(:invitation, team: team, user: new_user) }
+  # let(:new_user) { create(:user, email: "rookie@example.com") }
+  # let(:team) { create(:team, name: "A fine team") }
+  # let(:team_owner) { create(:user) }
 
-  before do
-    team.update!(owner: team_owner)
-    team_owner.update!(team: team)
+  # before do
+  #   team.update!(owner: team_owner)
+  #   team_owner.update!(team: team)
+  # end
+
+  def create_new_user
+    User.create(email: "rookie@example.com")
+  end
+
+  def create_team
+    Team.create(name: "A fine team")
+  end
+
+  def new_invite_for(new_user, team = nil)
+    Invitation.new(team: team, user: new_user)
   end
 
   describe "callbacks" do
     describe "after_save" do
       context "with valid data" do
         it "invites the user" do
+          new_user = create_new_user
+          team = create_team
+          invitation = new_invite_for(new_user, team)
+
           invitation.save
+
           expect(new_user).to be_invited
         end
       end
 
       context "with invalid data" do
-        before do
-          invitation.team = nil
-          invitation.save
-        end
+        # before do
+        #   invitation.team = nil
+        #   invitation.save
+        # end
 
         it "does not save the invitation" do
-          expect(invitation).not_to be_valid
-          expect(invitation).to be_new_record
+          new_user = create_new_user
+          invalid_invitation = new_invite_for(new_user)
+
+          invalid_invitation.save
+
+          expect(invalid_invitation).not_to be_valid
+          expect(invalid_invitation).to be_new_record
         end
 
         it "does not mark the user as invited" do
